a105a9ad5dfd2a5e88abcfeb7e2da09c07d63f04
nenad seke <nenad seke@localhost>
2011-10-12 00:51:42 +0200

    dodo jos nesto.

--- src/main.clj
+++ src/main.clj
@@ -190,18 +190,89 @@ nums
 (repeat 5 "hi")
 (range 5)
 
+(split-at 2 [:a :b :c :d :e :f])
+(split-with pos? [2 1 5 -3 6 -2 -1])
+(partition 2 [:a :b :c :d :e :f])
+(partition 2 1 [:a :b :c :d :e :f])
+(map pos? [2 1 5 -3 6 -2 -1])
+(map + [2 4 8] [1 3 5])
+(first [1 2 3 4])
+(second [1 2 3 4])
+(nth [:a :b :c :d] 2)
+(last [1 2 3 4])
+(reduce + [1 2 3 4 5])
+(apply + 1 [2 3])
+(apply + (range 1 6))
+(empty? [1 2 3 4])
+(empty? [])
+(some (fn [n] (< n 5)) [6 9 7 3])
+(some (fn [n] (< n 5)) [6 9 7 5])
+(every? (fn [n] (< n 5)) [2 1 4 3])
+(every? (fn [n] (< n 5)) [2 1 5 3])
 
+(def result (map println (range 1 5)))
+(dorun result)
+(doall result)
 
+;;STM
+(def my-ref (ref 5))
+(deref my-ref)
+@my-ref
+(+ 1 @my-ref)
+(dosync (alter my-ref + 3))
 
+;;Bank Accounts in STM
+(def account1 (ref 1000))
+(def account2 (ref 1500))
 
+(defn transfer "transfers amount of money from a to b" [a b amount]
+  (dosync
+    (alter a - amount)
+    (alter b + amount)))
 
+(transfer account1 account2 300)
+(transfer account2 account1 50)
 
+(println "Account #1:" @account1)
+(println "Account #2:" @account2)
 
+;;An Address Book in STM
+(def my-contacts (ref []))
 
+(defn add-contact "adds a contact to the provided contact list" [contacts contact]
+  (dosync
+    (alter contacts conj (ref contact))))
 
+(defn print-contacts "prints a list of contacts" [contacts]
+  (doseq [c @contacts]
+    (println (str "Name: " (@c :lname) ", " (@c :fname)))))
 
+(add-contact my-contacts {:fname "Luke" :lname "VanderHart"})
+(add-contact my-contacts {:fname "Stuart" :lname "Sierra"})
+(add-contact my-contacts {:fname "John" :lname "Doe"})
 
+(print-contacts my-contacts)
 
+;;Adding Initials to the Address Book
+(defn add-initials "adds initials to a single contact and returns it" [contact]
+  (assoc contact :initials
+         (str (first (contact :fname)) (first (contact :lname)))))
+
+(defn add-all-initials "adds initials to each of the contacts in a list of contacts" [contacts]
+  (dosync
+    (doseq [contact (ensure contacts)]
+      (alter contact add-initials))))
+
+(defn print-contacts-and-initials "prints a list of contacts, with initials" [contacts]
+  (dorun (map (fn [c]
+                (println (str "Name: " (c :lname) ", " (c :fname) " (" (c :initials) ")"))) @contacts)))
+
+(defn print-contacts-and-initials "prints a list of contacts, with initials" [contacts]
+  (doseq [c @contacts]
+    (println (str "Name: " (@c :lname) ", " (@c :fname) " (" (@c :initials) ")"))))
+
+(add-all-initials my-contacts)
+(print-contacts-and-initials my-contacts)
 
 
 
