e125c479f5352dae72701d7784dcdd638686071d
Nenad Seke <nseke@dmvdesk.com>
2016-03-10 18:19:59 -0800

    izmena

--- src/qwerty/csv_manipulation.clj
+++ src/qwerty/csv_manipulation.clj
@@ -1,15 +1,12 @@
 (ns qwerty.csv-manipulation
   (require [semantic-csv.core :refer :all]
            [clojure-csv.core :as csv]
-           [clojure.java.io :as io]))
+           [clojure.java.io :as io]
+           [clojure.core.matrix :as mtx]
+           [clojure.math.combinatorics :as cmb]))
 
-(def old-file "c:/Users/user/Google Drive/imports/20160309_requestId_676_null_DEALS.csv")
-(def new-file "c:/Users/user/Google Drive/imports/20160309_requestId_675_null_DEALS.csv")
-
-(with-open [in-file (io/reader old-file)]
-  (->>
-    (csv/parse-csv in-file)
-    first))
+(def old-file "d:/ftp/24589/20160310_requestId_692_null_DEALS-old.csv")
+(def new-file "d:/ftp/24589/20160310_requestId_693_null_DEALS-new.csv")
 
 (defn get-headers 
   "return vector of strings"
@@ -52,21 +49,92 @@
   #(not (in? (get-headers new-file) % ))
   (get-headers old-file)) 
 
-
 (with-open [in-file (io/reader old-file)]
   (->>
     (csv/parse-csv in-file)
     mappify
-    doall
-    second))
+    doall))
 
-(def sample-file "d:/ftp/24589/sample.csv")
+(def a [[ 1   2   0]
+        ["4" "5" "6"] 
+        ["7" "8" "9"]
+        ["10" "11" "12"]
+        ["a" "b" "c"]]) 
+ 
+(def b [[ 1   2   "w"] 
+        ["4" "5" "6"] 
+        ["7" "8" "9"]
+        ["10" "11" "12"]
+        ["a" "b" "c"]])
 
-(with-open [in-file (io/reader sample-file)]
-  (->>
-    (csv/parse-csv in-file)
-    mappify
-    doall
-    count))
+ (defn compare [a b combination]
+  (if (= a b)
+    nil
+    [a b combination]))
+ (defn differences [matrix-a matrix-b]
+ (let [witdh (first (mtx/shape matrix-a))
+       length (second (mtx/shape matrix-a))
+       all-coordinates (cmb/cartesian-product (range witdh) (range length))]
+   (filter
+     #(not (nil? %))
+     (map #(compare
+           (mtx/mget matrix-a (first %) (second %))
+           (mtx/mget matrix-b (first %) (second %))
+           %) 
+        all-coordinates))))
+
+(differences a b)
+
+(with-open [old-file (io/reader old-file) new-file (io/reader new-file)]
+    (let [old-rows (doall (csv/parse-csv old-file))
+          new-rows (doall (csv/parse-csv new-file))]
+      (println "differences in matrixes cells (but we are not comparing first row) ="
+               (differences 
+                 (rest old-rows) 
+                 (rest new-rows)))
+      (str "matrixes are same dimenzions = " (= 
+                                              (mtx/shape old-rows)
+                                              (mtx/shape new-rows)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
