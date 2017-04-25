25ccf1b0cc77dc65bb902a3fdd4ed90a35e0ff39
Nenad Seke <nenad.seke@vicert.com>
2017-04-25 15:23:05 +0200

    lazy seq

--- src/qwerty/brave_ch4.clj
+++ src/qwerty/brave_ch4.clj
@@ -42,3 +42,77 @@
 
 (my-map inc human-consumption)
 
+(take 3 [1 2 3 4 5 6 7 8 9 10])
+
+(drop 3 [1 2 3 4 5 6 7 8 9 10])
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+
+(drop-while #(< (:month %) 3) food-journal)
+
+(take-while #(< (:month %) 4)
+  (drop-while #(< (:month %) 2) food-journal))
+
+(and nil "ovaj string nece biti vracen")
+
+(and false "ovaj string nece biti vracen")
+
+(and true "ovaj string je vracen")
+
+(and "some value" "ovaj string je vracen")
+
+(and :asdf "ovaj string je vracen")
+
+(some #(and (> (:critter %) 3) %) food-journal)
+
+(sort [3 1 2])
+
+(sort ["aaa" "c" "bb"])
+
+(sort-by count ["aaa" "c" "bb"])
+
+(concat [1 2] [3 4])
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+(time (vampire-related-details 0))
+
+(time (def mapped-details (map vampire-related-details (range 0 1000000))))
+
+(time (first mapped-details))
+
+(time (first mapped-details))
+
+(time (identify-vampire (range 0 1000000)))
+
+
